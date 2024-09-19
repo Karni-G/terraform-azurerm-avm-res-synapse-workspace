2a817dbde62e120df48682d5b55056fe477216a7
Karni Gupta <78787142+Karni-G@users.noreply.github.com>
2024-09-19 22:04:13 +0530

    dummy for github repo added

---
+++ examples/default-with-azure-github-repo/variables.tf
@@ -0,0 +1,34 @@
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see <https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+}
+
+variable "storage_data_lake_gen2_filesystem_name" {
+  type = string
+  description = "Specifies the name of storage data lake gen2 filesystem resource."
+}
+
+variable "sql_administrator_login" {
+  type = string
+  default = "SQLAdmin"
+  description = "Specifies The login name of the SQL administrator. Changing this forces a new resource to be created. If this is not provided customer_managed_key must be provided. "
+}
+
+variable "sql_administrator_login_password" {
+  type = string
+  sensitive = true
+  default = "null"
+  description = "The Password associated with the sql_administrator_login for the SQL administrator. If this is not provided customer_managed_key must be provided."
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "The map of tags to be applied to the resource"
+}
+
